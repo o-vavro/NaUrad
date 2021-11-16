a1bdb4be4c19ea974c6bbe0a374276e9a5b70281
Ondrej Vavro <o_vavro@utb.cz>
2021-11-16 01:07:06 +0100

    Current location and location permissions.

--- app/src/main/java/com/atlasstudio/naurad/MainActivity.kt
+++ app/src/main/java/com/atlasstudio/naurad/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.atlasstudio.naurad
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.atlasstudio.naurad.databinding.ActivityMapsBinding
@@ -10,17 +12,21 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+
+const val REQUEST_CODE_LOCATION = 123
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
+    private lateinit var mBinding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        mBinding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -32,10 +38,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
         mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(
             this, R.raw.mapstyle))
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isZoomGesturesEnabled = true
+
+        enableCurrentLocation()
 
         // Add a marker in Zlin and move the camera
         val zlin = LatLng(49.230505, 17.657103)
-        mMap.addMarker(MarkerOptions().position(zlin).title("Marker in Zlin"))
+        mMap.addMarker(MarkerOptions().position(zlin).title(getString(R.string.default_map_marker)))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zlin, 14.5f))
+    }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(REQUEST_CODE_LOCATION)
+    private fun enableCurrentLocation() {
+        if (hasLocationPermission()) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.location_permissions),
+                REQUEST_CODE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)
     }
 }
